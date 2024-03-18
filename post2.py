2bd463d3c01c8d1daa6d1b2af2de5dea680565fc
Samarth Upadhyay <126957213+samarthupa@users.noreply.github.com>
2024-03-18 17:06:54 +0530

    Update post2.py

--- post2.py
+++ post2.py
@@ -5,7 +5,7 @@ import pandas as pd
 from urllib.parse import urlparse
 
 def get_search_results(keyword):
-    url = f"https://www.google.com/search?q={'+'.join(keyword.split())}&num=60"
+    url = f"https://www.google.co.in/search?q={'+'.join(keyword.split())}&num=60"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107 Safari/537",
     }
@@ -19,10 +19,13 @@ def get_search_results(keyword):
 def find_domain_ranking(html_content, domain):
     soup = BeautifulSoup(html_content, 'html.parser')
     results = soup.find_all('div', class_='tF2Cxc')
+    urls_ranking = []
     for i, result in enumerate(results, start=1):
+        url = result.find('a')['href']
+        urls_ranking.append(url)
         if domain.lower() in result.get_text().lower():
-            return i
-    return None
+            return i, urls_ranking
+    return None, urls_ranking
 
 def clean_domain(domain):
     parsed_domain = urlparse(domain)
@@ -54,15 +57,15 @@ def main():
         for keyword in keywords_list:
             search_results = get_search_results(keyword)
             if search_results:
-                ranking = find_domain_ranking(search_results, clean_domain(domain))
+                ranking, urls_ranking = find_domain_ranking(search_results, clean_domain(domain))
                 if ranking:
-                    data.append([keyword, ranking])
+                    data.append([keyword, ranking, urls_ranking])
                 else:
-                    data.append([keyword, "Not Found"])
+                    data.append([keyword, "Not Found", urls_ranking])
             else:
-                data.append([keyword, "Failed"])
+                data.append([keyword, "Failed", urls_ranking])
 
-        df = pd.DataFrame(data, columns=["Keyword", "Ranking"])
+        df = pd.DataFrame(data, columns=["Keyword", "Ranking", "URLs Ranking"])
         st.table(df)
 
 if __name__ == "__main__":
